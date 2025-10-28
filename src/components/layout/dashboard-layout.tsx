96c6d47ff5a16dc117411fd417b6f40bee17f92a
Matthew Jerico Silva <matthewjerico.silva@dict.gov.ph>
2025-10-28 14:00:17 +0800

    Remove top padding from page content container

--- src/components/layout/dashboard-layout.tsx
+++ src/components/layout/dashboard-layout.tsx
@@ -181,7 +181,7 @@ export function DashboardLayout({ children }: { children: React.ReactNode }) {
         )}
       >
         {/* Page content */}
-        <div className="w-full max-w-[1920px] mx-auto px-4 sm:px-6 lg:px-8 pb-4 sm:pb-6 lg:pb-8 pt-1">{children}</div>
+        <div className="w-full max-w-[1920px] mx-auto px-4 sm:px-6 lg:px-8 pb-4 sm:pb-6 lg:pb-8">{children}</div>
       </div>
     </>
   );
