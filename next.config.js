f71c4daa3b8af40d257ac367547fe10c478aa860
Matthew Jerico Silva <matthewjerico.silva@dict.gov.ph>
2025-10-30 10:48:06 +0800

    Update CSP to allow Google Docs viewer and Cloud Storage iframes

--- next.config.js
+++ next.config.js
@@ -40,7 +40,7 @@ const nextConfig = {
           },
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data:; connect-src 'self' https:; frame-ancestors 'self';"
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data:; connect-src 'self' https:; frame-src 'self' https://docs.google.com https://storage.googleapis.com; frame-ancestors 'self';"
           }
         ],
       },
